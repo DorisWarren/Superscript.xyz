ea25e8b387e1e3669dc8b12a03b3c8891ef7b8d7
DorisWarren <doriswarren11@gmail.com>
2020-04-10 14:26:36 -0700

    iframe responsive is now responsive

--- src/pages/brand.js
+++ src/pages/brand.js
@@ -1,17 +1,13 @@
 import React from "react"
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const BrandPage = () => (
-  <Layout>
-    <SEO title="Brand" />
-    <h2>Banding</h2>
-    <div className="brand">
-    <iframe src="https://published.1brand.co/brand/guidelines/superscript-xyz 
-" width="800" height="600"></iframe>
 
-    </div>
-  </Layout>
+  <div className="iframe-container">
+    <SEO title="Brand" />
+    <iframe title="brand-guidelines" src="https://published.1brand.co/brand/guidelines/superscript-xyz" ></iframe>
+  </div>
+
 )
 
 export default BrandPage;
